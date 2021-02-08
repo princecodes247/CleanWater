a64fc215b4f55fe2209eea8da8cd09a753636edb
Prince Codes 247 <princecodes247@gmail.com>
2021-02-08 11:01:32 -0800

    feat: update about route

--- routes/about-us.js
+++ routes/about-us.js
@@ -5,10 +5,10 @@ router.get("/", (req, res) => {
   res.render("about-us/about-us");
 });
 router.get("/founders-board-team", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/founders-board-team");
 });
 router.get("/why-water", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/why-water");
 });
 router.get("/our-vision", (req, res) => {
   res.render("about-us/our-vision");
@@ -17,22 +17,22 @@ router.get("/ways-donate", (req, res) => {
   res.render("about-us/ways-donate");
 });
 router.get("/ways-donate/start-fundraiser", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/ways-donate/start-fundraiser");
 });
 router.get("/ways-donate/shop-support", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/ways-donate/shop-support");
 });
 router.get("/ways-donate/cause-marketing", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/ways-donate/cause-marketing");
 });
 router.get("/resources", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/resources");
 });
 router.get("/news-press", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/news-press");
 });
 router.get("/financials", (req, res) => {
-  res.render("about-us/about-us");
+  res.render("about-us/financials");
 });
 
 module.exports = router;
